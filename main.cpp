ae7dcdd79b85d9a743ad2813f7e9ebd3a27545e0
schoolover <106459107+schoolover@users.noreply.github.com>
2023-02-26 11:54:29 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+     MainWindow w;
+    w.setWindowTitle("变结构petri网建模工具软件");
+    w.setMinimumSize(1440,900);
+    w.showMaximized();
+    w.show();
+    return a.exec();
+}
